9fb53d6f0e8a49bc16e446b1111f4b4bea3040fb
Basel5050 <baselashraf1@gmail.com>
2025-04-09 12:49:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Button } from "@material-tailwind/react";
+
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1  className="text-3xl  font-bold underline">
+    Hello world!
+  </h1>
+  <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
